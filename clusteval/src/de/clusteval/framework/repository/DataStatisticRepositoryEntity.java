4b075ea77c54c3604456b0dc081d95c10cd2197f
wiwie <derwiwie@googlemail.com>
2013-12-08 18:27:49 +0100

    started refactoring of Repository, to reduce code duplication; refactored Statistic classes

---
+++ clusteval/src/de/clusteval/framework/repository/DataStatisticRepositoryEntity.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package de.clusteval.framework.repository;
+
+import java.util.Map;
+
+import de.clusteval.data.statistics.DataStatistic;
+import de.clusteval.data.statistics.DataStatisticCalculator;
+import de.clusteval.run.statistics.RunDataStatistic;
+import de.clusteval.run.statistics.RunDataStatisticCalculator;
+
+/**
+ * @author Christian Wiwie
+ * 
+ */
+public class DataStatisticRepositoryEntity
+		extends
+			DynamicRepositoryEntity<DataStatistic> {
+
+	/**
+	 * A map containing all classes of data statistic calculators registered in
+	 * this repository.
+	 */
+	protected Map<String, Class<? extends DataStatisticCalculator<? extends DataStatistic>>> dataStatisticCalculatorClasses;
+
+	/**
+	 * @param repository
+	 * @param parent
+	 * @param basePath
+	 */
+	public DataStatisticRepositoryEntity(Repository repository,
+			DataStatisticRepositoryEntity parent, String basePath) {
+		super(repository, parent, basePath);
+	}
+
+	/**
+	 * This method looks up and returns (if it exists) the class of the data
+	 * statistic calculator for the datastatistic class with the given name.
+	 * 
+	 * @param dataStatisticClassName
+	 *            The name of the datastatistic class.
+	 * 
+	 * @return The class of the data statistic calculator with the given name or
+	 *         null, if it does not exist.
+	 */
+	public Class<? extends DataStatisticCalculator<? extends DataStatistic>> getDataStatisticCalculator(
+			final String dataStatisticClassName) {
+		Class<? extends DataStatisticCalculator<? extends DataStatistic>> result = this.dataStatisticCalculatorClasses
+				.get(dataStatisticClassName);
+		if (result == null && parent != null)
+			result = ((DataStatisticRepositoryEntity) this.parent)
+					.getDataStatisticCalculator(dataStatisticClassName);
+		return result;
+	}
+
+	public boolean registerDataStatisticCalculator(
+			Class<? extends DataStatisticCalculator<? extends DataStatistic>> dataStatisticCalculator) {
+		return this.dataStatisticCalculatorClasses.put(dataStatisticCalculator
+				.getName().replace("Calculator", ""), dataStatisticCalculator) != null;
+	}
+}
