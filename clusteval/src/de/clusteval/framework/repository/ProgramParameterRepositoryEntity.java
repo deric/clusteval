5747d81928d298f7570d21c7773a526f04030299
wiwie <derwiwie@googlemail.com>
2013-12-10 00:08:45 +0100

    started refactoring of Repository, to reduce code duplication

---
+++ clusteval/src/de/clusteval/framework/repository/ProgramParameterRepositoryEntity.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package de.clusteval.framework.repository;
+
+import de.clusteval.program.ProgramParameter;
+
+/**
+ * @author Christian Wiwie
+ * 
+ */
+public class ProgramParameterRepositoryEntity<T extends ProgramParameter<?>>
+		extends
+			StaticRepositoryEntity<T> {
+
+	/**
+	 * @param repository
+	 * @param parent
+	 * @param basePath
+	 */
+	public ProgramParameterRepositoryEntity(Repository repository,
+			StaticRepositoryEntity<T> parent, String basePath) {
+		super(repository, parent, basePath);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.clusteval.framework.repository.StaticRepositoryEntity#registerWhenExisting
+	 * (de.clusteval.framework.repository.RepositoryObject,
+	 * de.clusteval.framework.repository.RepositoryObject)
+	 */
+	@Override
+	protected <S extends T> boolean registerWhenExisting(S old, S object)
+			throws RegisterException {
+		return false;
+	}
+}
