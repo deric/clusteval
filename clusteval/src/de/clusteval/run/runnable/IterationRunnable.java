abc6ac3d5e3ad48b863821d5ef8f8ace476c1e36
wiwiec <derwiwie@googlemail.com>
2014-04-16 15:57:01 +0200

    added IterationRunnable class, whcih encapsulates execution of a clsutering iteration

---
+++ clusteval/src/de/clusteval/run/runnable/IterationRunnable.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package de.clusteval.run.runnable;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.clusteval.framework.RLibraryNotLoadedException;
+import de.clusteval.run.result.NoRunResultFormatParserException;
+import de.clusteval.utils.RNotAvailableException;
+
+/**
+ * @author Christian Wiwie
+ * 
+ */
+public abstract class IterationRunnable implements Runnable {
+
+
+	protected Logger log;
+	protected IterationWrapper iterationWrapper;
+	protected NoRunResultFormatParserException noRunResultException;
+	protected IOException ioException;
+	protected RLibraryNotLoadedException rLibraryException;
+	protected RNotAvailableException rNotAvailableException;
+
+	public IterationRunnable(final IterationWrapper iterationWrapper) {
+		super();
+		this.iterationWrapper = iterationWrapper;
+		this.log = LoggerFactory.getLogger(getClass());
+	}
+
+	/**
+	 * @return the noRunResultException
+	 */
+	public NoRunResultFormatParserException getNoRunResultException() {
+		return noRunResultException;
+	}
+
+	/**
+	 * @return the rNotAvailableException
+	 */
+	public RNotAvailableException getrNotAvailableException() {
+		return rNotAvailableException;
+	}
+
+	/**
+	 * @return the ioException
+	 */
+	public IOException getIoException() {
+		return ioException;
+	}
+
+	/**
+	 * @return the rLibraryException
+	 */
+	public RLibraryNotLoadedException getrLibraryException() {
+		return rLibraryException;
+	}
+}
